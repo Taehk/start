d59defd01e90e86b7b0d4e3df63d15b77b68c10c
Taehk <taehk1013@gmail.com>
2023-10-31 10:07:29 +0900

    cpubound

---
+++ src/main/java/com/playdata/cpubouindapp/CpubouindappApplication.java
@@ -0,0 +1,13 @@
+package com.playdata.cpubouindapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpubouindappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpubouindappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/playdata/cpubouindapp/CpubouindappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.cpubouindapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpubouindappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
